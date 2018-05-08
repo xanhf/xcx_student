16e07501d7b498fba0dc375876bf6fb4ec99d243
xanhf <1358588411@qq.com>
2018-05-08 21:08:27 +0800

    跳转逻辑完善

--- client/pages/videoDetail/videoDetail.js
+++ client/pages/videoDetail/videoDetail.js
@@ -13,7 +13,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let item = options.item;
+    let item = JSON.parse(options.item);
     this.data.info = item;
     this.videoPnum(item.id);
   },
